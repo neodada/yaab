01e69d97c54dbf190ffc2ccbc7a8e016ad044a61
mccloud <av@gyrus.biz>
2012-11-06 00:55:43 +0200

    release 1.6.1 - fixed Spanish translation

--- src/biz/gyrus/yaab/RangesActivity.java
+++ src/biz/gyrus/yaab/RangesActivity.java
@@ -7,6 +7,7 @@ import java.util.Observer;
 import biz.gyrus.yaab.BrightnessController.BrightnessStatus;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -109,6 +110,7 @@ public class RangesActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.avtivity_ranges);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		
 		_sbMin = (SeekBar) findViewById(R.id.sbMin);
 		_sbMax = (SeekBar) findViewById(R.id.sbMax);
